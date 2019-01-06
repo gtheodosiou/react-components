8066856639d5baf29fe9127960faa64101ec1c73
George <theodgeorge@gmail.com>
2019-01-06 19:52:15 +0000

    Use props to pass values

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import faker from "faker";
 
 const App = () => {
     return (
       <div className="ui container comments">
-          <CommentDetail/>
+          <CommentDetail
+              author="George"
+              timeAgo="Today at 4:45PM"
+              content="Nice post"
+              avatar={faker.image.avatar()}
+          />
+          <CommentDetail
+              author="Panos"
+              timeAgo="Today at 3:00AM"
+              content="Better post"
+              avatar={faker.image.avatar()}
+          />
+          <CommentDetail
+              author="Sonia"
+              timeAgo="Yesterday at 7:45AM"
+              content="The best post"
+              avatar={faker.image.avatar()}
+          />
       </div>
     );
 };
