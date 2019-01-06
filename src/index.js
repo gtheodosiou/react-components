14bd873668a09d104dd059959d0ba12f93494308
George <theodgeorge@gmail.com>
2019-01-06 12:38:03 +0000

    Add 2 more comments. Looking bad

--- src/index.js
+++ src/index.js
@@ -15,7 +15,41 @@ const App = () => {
                       George
                   </a>
                   <div className="metadata">
-                      <span className="date"> Today at 6:00 PM</span>
+                      <span className="date">Today at 6:00 PM</span>
+                  </div>
+                  <div className="text">
+                      Nice blog post!
+                  </div>
+              </div>
+          </div>
+
+          <div className="comment">
+              <a href="/"  className="avatar">
+                  <img alt="avatar" src={faker.image.avatar()}/>
+              </a>
+              <div className="content">
+                  <a href="/"  className="author">
+                      George
+                  </a>
+                  <div className="metadata">
+                      <span className="date">Today at 6:00 PM</span>
+                  </div>
+                  <div className="text">
+                      Nice blog post!
+                  </div>
+              </div>
+          </div>
+
+          <div className="comment">
+              <a href="/"  className="avatar">
+                  <img alt="avatar" src={faker.image.avatar()}/>
+              </a>
+              <div className="content">
+                  <a href="/"  className="author">
+                      George
+                  </a>
+                  <div className="metadata">
+                      <span className="date">Today at 6:00 PM</span>
                   </div>
                   <div className="text">
                       Nice blog post!
